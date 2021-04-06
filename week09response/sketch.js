38e29ca5f0b4b3a39c86f65b154c39ed2e1e6619
kyllesu <68802955+kyllesu@users.noreply.github.com>
2021-04-06 11:53:22 -0700

    updated page

--- week09response/sketch.js
+++ week09response/sketch.js
@@ -6,7 +6,26 @@ function redirect() {
 
 }
 
+var modal = document.getElementById("myModal");
 
-    
-    
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+//open popup
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+//close popup
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// click out window
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
